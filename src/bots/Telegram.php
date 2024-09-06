f5ecdb216edd46e49e9717ccdfdec3afa1862d8a
Picus <picus.2022@mail.ru>
2024-09-06 11:47:20 +0300

    add tg lib

--- src/bots/Telegram.php
+++ src/bots/Telegram.php
@@ -2,8 +2,36 @@
 
 namespace marksync_libs\bots;
 
-abstract class Telegram {
+
+/**
+ * https://core.telegram.org/methods
+ */
+abstract class Telegram
+{
+
+    protected int $chatId;
+    protected string $token;
 
 
+    /** 
+     * https://core.telegram.org/method/messages.sendMessage
+     */
+    function send(string $message)
+    {
+        return $this->fetch(
+            'sendMessage',
+            chat_id: $this->chatId,
+            text: $message,
+        );
+    }
 
+
+    function fetch(string $method, ...$props)
+    {
+        $params = http_build_query($props);
+        $result = file_get_contents("https://api.telegram.org/bot{$this->token}/{$method}?$params");
+
+        if ($result)
+            return json_decode($result, true);
+    }
 }
