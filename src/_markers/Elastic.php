9420e6274e6deaffbd05fbfcade05a2708e702ff
X <no_soap_0@mail.ru>
2024-09-19 12:26:30 +0300

    mark

--- src/_markers/Elastic.php
+++ src/_markers/Elastic.php
@@ -2,20 +2,20 @@
 namespace marksync_libs\_markers;
 use marksync\provider\provider;
 use marksync_libs\Elastic\ElasticIndex;
-use marksync_libs\Elastic\Search\Search;
 use marksync_libs\Elastic\Search\IndexInstance;
+use marksync_libs\Elastic\Search\Search;
 
 /**
  * @property-read ElasticIndex $elasticIndex
- * @property-read Search $search
  * @property-read IndexInstance $index
+ * @property-read Search $search
 
 */
 trait Elastic {
     use provider;
 
    function createElasticIndex(): ElasticIndex { return new ElasticIndex; }
-   function createSearch(): Search { return new Search($this); }
    function createIndex(): IndexInstance { return new IndexInstance($this); }
+   function createSearch(): Search { return new Search($this); }
 
 }
